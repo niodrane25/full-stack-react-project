53daf8176efe1e2d2bc5251efc5905ddf5160ca5
niodrane25 <adrian.h.pacheco7@gmail.com>
2023-06-12 11:19:17 -0400

    Routes added, tables added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import postgres from "postgres";
 import dotenv from "dotenv";
 
+
 dotenv.config({ path: "../.env" });
 
 const PORT = process.env.PORT;
@@ -10,12 +11,82 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/tasks", (req, res) => {
-  sql`SELECT * FROM tasks`.then((rows) => {
-    res.send(rows);
-  });
+// Route for user registration
+app.post("/api/register", (req, res) => {
+  const { username, email, password } = req.body;
+
+  sql`
+    INSERT INTO users (username, email, password)
+    VALUES (${username}, ${email}, ${password})
+    RETURNING *
+  `
+    .then((result) => {
+      const newUser = result[0];
+      // Handle the response or send a success message
+      res.send(newUser);
+    })
+    .catch((error) => {
+      // Handle errors and send an appropriate error response
+      res.status(500).send("Error registering user");
+    });
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+// Route for profile creation
+app.post("/api/profiles", (req, res) => {
+  const { user_id, user_name, email, bio } = req.body;
+
+  sql`
+    INSERT INTO profiles (user_id, user_name, email, bio, created_at, updated_at)
+    VALUES (${user_id}, ${user_name}, ${email}, ${bio}, NOW(), NOW())
+    RETURNING *
+  `
+    .then((result) => {
+      const newProfile = result[0];
+      // Handle the response or send a success message
+      res.send(newProfile);
+    })
+    .catch((error) => {
+      // Handle errors and send an appropriate error response
+      res.status(500).send("Error creating profile");
+    });
+});
+
+// Route for post creation
+app.post("/api/posts", (req, res) => {
+  const { user_id, content } = req.body;
+
+  sql`
+    INSERT INTO posts (user_id, content, created_at, updated_at)
+    VALUES (${user_id}, ${content}, NOW(), NOW())
+    RETURNING *
+  `
+    .then((result) => {
+      const newPost = result[0];
+      // Handle the response or send a success message
+      res.send(newPost);
+    })
+    .catch((error) => {
+      // Handle errors and send an appropriate error response
+      res.status(500).send("Error creating post");
+    });
+});
+
+// Route for post listing
+app.get("/api/posts", (req, res) => {
+  sql`
+    SELECT * FROM posts
+  `
+    .then((result) => {
+      const posts = result;
+      // Handle the response or send the posts
+      res.send(posts);
+    })
+    .catch((error) => {
+      // Handle errors and send an appropriate error response
+      res.status(500).send("Error retrieving posts");
+    });
+});
+
+app.listen(5173, () => {
+  console.log(`Listening on port 5173`);
 });
--- server/migration.sql
+++ server/migration.sql
@@ -1,12 +1,36 @@
-DROP TABLE IF EXISTS tasks;
+-- DROP TABLE IF EXISTS profiles;
 
-CREATE TABLE tasks (
-  id SERIAL,
-  description TEXT
+-- migration.sql
+
+CREATE TABLE users (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO tasks(description) VALUES('Do the dishes');
-INSERT INTO tasks(description) VALUES('Walk the dog');
-INSERT INTO tasks(description) VALUES('Sweep the floor');
-INSERT INTO tasks(description) VALUES('Do your homework');
-INSERT INTO tasks(description) VALUES('Beat Elden Ring');
+CREATE TABLE profiles (
+  user_id SERIAL PRIMARY KEY,
+  user_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  bio TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE posts (
+  post_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL,
+  content TEXT NOT NULL,
+  likes_count INT DEFAULT 0,
+  comments_count INT DEFAULT 0,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES users (user_id)
+);
+
+
+-- INSERT INTO profiles(description) VALUES('Do the dishes');
+
